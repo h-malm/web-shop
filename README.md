89e246c9c457aec73be7562180aecb824e776d6a
Hilda Malm <113290077+h-malm@users.noreply.github.com>
2026-03-02 11:35:13 +0200

    all files probably

---
+++ README.md
@@ -0,0 +1,30 @@
+```
+WEB TECHNOLOGIES
+
+Hilda Malm, hilda.malm@abo.fi
+
+---------------------------------------------------------
+Constraints: 1 2 3 
+
+---------------------------------------------------------
+Requirements: 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16, non-functional
+
+---------------------------------------------------------
+Run the project:
+
+cd to root of project
+pip install -r requirements.txt
+
+cd to backend
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver
+
+cd to frontend
+nvm install v18.17.0
+npm install --include-dev
+npm run dev
+
+then navigate to localhost:3000
+
+```
---
+++ frontend/src/app/components/Card.js
@@ -0,0 +1,62 @@
+import { Button } from "primereact/button";
+import Label from "./Label";
+import Square from "./Square";
+import styles from './../page.module.css';
+import React from "react";
+
+export default function Card ( props ) {
+	const handleClick = () => {
+		if ( props.clickHandler ) {
+			props.clickHandler();
+		}
+	};
+
+	const handleEdit = () => {
+		if ( props.editHandler ) {
+			props.editHandler();
+		}
+	};
+
+	const handleDelete = () => {
+		if ( props.deleteHandler ) {
+			props.deleteHandler();
+		}
+	};
+
+	return (
+		<div className={styles.cardStyle} onClick={handleClick}>
+			<Square sColor={props.cColor} />
+			<Label
+				lColor={props.cColor}
+				lOwner={props.cOwner}
+				lPrice={props.cPrice}
+				lTitle={props.cTitle}
+				lDateAdded={props.cDateAdded}
+				lDescription={props.cDescription}
+				lStatus={props.cStatus}
+			/>
+			<div className={styles.pairOfButtons}>
+				{!props.isEditable && (
+					<Button
+						label="Add to Cart"
+						className="p-button 	saveButton"
+					/>
+				)}
+				{props.isEditable && (
+					<Button
+						label='Edit'
+						onClick={handleEdit}
+						className="p-button saveButton"
+					/>
+				)}
+				{props.isDeletable && ( <Button
+					severity="danger"
+					label="Delete"
+					onClick={handleDelete}
+					className="p-button cancelButton"
+				/>
+				)}
+			</div>
+		</div>
+	);
+}
