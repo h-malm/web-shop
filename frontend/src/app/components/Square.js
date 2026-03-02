ae678dedbf183421648a810e1a87e137561305a8
Hilda Malm <113290077+h-malm@users.noreply.github.com>
2026-03-02 11:45:54 +0200

    fixed faulty structure

--- frontend/src/app/components/Square.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './../page.module.css';
-export default function Square ( props ) {
-	const sStyle = {
-		backgroundColor: props.sColor,
-	};
-	return <div style={sStyle} className={styles.squareInBrowse}></div>;
-}
--- frontend/src/utils/api.js
+++
@@ -1,2 +0,0 @@
-// export const API_BASE = "http://localhost:8000/api"
-export const API_BASE = "/api"
